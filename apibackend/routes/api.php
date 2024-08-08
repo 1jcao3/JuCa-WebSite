bbf9017f5bde01221882a5390bbd298568dc98a5
J cao <juliankamilo6@gmail.com>
2024-08-08 12:18:37 -0500

    Actualizar configuración de rutas de la API

--- apibackend/routes/api.php
+++ apibackend/routes/api.php
@@ -3,5 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
-Route::get('/projects', [ProjectController::class, 'index']);
-Route::get('/projects/{id}', [ProjectController::class, 'show']);
+
+Route::prefix('api')->group(function () {
+    Route::get('/projects', [ProjectController::class, 'index']);
+    Route::get('/projects/{id}', [ProjectController::class, 'show']);
+});
