834b076d114738578d76d01289a1d1d37cc076b6
J cao <juliankamilo6@gmail.com>
2024-08-08 19:01:10 -0500

    updating hacces

--- apibackend/routes/api.php
+++ apibackend/routes/api.php
@@ -3,8 +3,5 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
-
-Route::prefix('api')->group(function () {
-    Route::get('/projects', [ProjectController::class, 'index']);
-    Route::get('/projects/{id}', [ProjectController::class, 'show']);
-});
+Route::get('/projects', [ProjectController::class, 'index']);
+Route::get('/projects/{id}', [ProjectController::class, 'show']);
